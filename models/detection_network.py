7b8b2063472ba7dd18a88ce6db183973ae043997
Iliass b <iliass.bourabaa@usherbrooke.ca>
2023-02-16 20:46:08 -0500

    Detection still in progress

--- models/detection_network.py
+++ models/detection_network.py
@@ -6,9 +6,9 @@ import torchvision
 class AlexNetDectection(nn.Module):
     def __init__(self):
         super(AlexNetDectection, self).__init__()
-        self.hidden = 32
+        self.hidden = 16
         self.extraction = nn.Sequential(
-            nn.Conv2d(1, 2*self.hidden, kernel_size=5, stride=1, padding=1),
+            nn.Conv2d(1, 2*self.hidden, kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=1),
             nn.Conv2d(2*self.hidden, 4*self.hidden, kernel_size=3, padding=1),
@@ -22,34 +22,24 @@ class AlexNetDectection(nn.Module):
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=3, stride=1),
         )
-        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
         self.classi_detect = nn.Sequential(
-            nn.Linear(1152, 2*self.hidden),
+            nn.Linear(8464, 2*self.hidden),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
             nn.Linear(2*self.hidden, 2*self.hidden),
             nn.ReLU(inplace=True),
-            nn.Linear(2*self.hidden, 7),
+            nn.Linear(2*self.hidden, 3*7),
         )
-        self.classi = nn.Sigmoid()
-        self.detect = nn.Sigmoid()
+        self.softmax = nn.Softmax()
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, input):
         x = self.extraction(input)
-        x = self.avgpool(x)
         x = torch.flatten(x, 1)
         x = self.classi_detect(x)
-        x_classi = self.classi(torch.narrow(x,1,0,3))
-        x_detect = self.detect(torch.narrow(x,1,3,4))
+        x = self.sigmoid(x)
 
-        output = torch.zeros([len(x), 3, 5])
-
-
-        for i in range(len(x)):
-            for j in range(len(x_classi[0])):
-                for k in range(len(x_detect[0])):
-                    output[i][j][1+k] = x_detect[i][k]
-                output[i][j][0] = x_classi[i][j]
+        output = x.reshape(len(x), 3, 7)
 
         return output
 
@@ -69,10 +59,10 @@ class DetectionCriterion(nn.Module):
             for j in range(len(target[0])):
                 targetClasse[i, j, int(target[i, j, -1])] = 1
 
-        xywhLoss = self.criterionMSE(output[:,:,1:4], target[:,:,1:4])
-        #classLoss = self.criterionBCE(output[:,:,:3], targetClasse)
+        xywhLoss = self.criterionMSE(output[:,:,3:6], target[:,:,1:4])
+        classLoss = self.criterionBCE(output[:,:,:3], targetClasse)
         objectLoss = self.criterionBCE(output[:,:,0], targetObject)
         noObjectLoss = self.criterionBCE(output[:,:,0], targetNoObject)
-        criterion = 5*xywhLoss + 0.5*noObjectLoss + objectLoss #classLoss + 5*xywhLoss + 0.5*noObjectLoss + objectLoss
+        criterion = 2*classLoss + 5*xywhLoss + 0.5*noObjectLoss + objectLoss
         return criterion
 
